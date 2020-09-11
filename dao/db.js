4562b64135c54ab61db8758d14676f99ae6ffcdb
Gregory Araújo <gregcjni@gmail.com>
2020-09-11 02:20:45 -0300

    Update Modelagem DB
    Finalização das modelagens de banco de dados e criação automática de tabelas

--- dao/db.js
+++ dao/db.js
@@ -1,84 +1,81 @@
 const Sequelize = require('sequelize');
 const dbName = "fr_test_gregory" //criação de um nome que acredito não conflitar com nenhum outro banco
 
-//função que irá gerenciar o acesso ao banco de dados, preparando a conexão quando ela não existir
 function connectDataBase() {
-
     if (!global.connection) { //testa se a conexão está ativa, se não estiver, monta
-
         global.connection = new Sequelize("", process.env.DBUSER, process.env.DBPASS,
-            { host: "localhost", dialect: 'mysql' })
-
-        global.connection.authenticate()
-            .then(function () {
-                console.log("Connected to Mysql.");
-            })
-            .catch(error => {
-                console.log("Not possible to connect to Database. " + error);
-                global.connection = null;
-                /*aqui eu faço a limpeza do baco de dados para que caso o banco tenha
-                sido desconectado durante o uso da aplicação e venha a ser reconectado, 
-                a conexão seja reiniciada na próxima chamada */
-            })
+            { host: "localhost", dialect: 'mysql' });
+        return global.connection.authenticate();
     }
 }
 
-//função para encerrar conexão com banco de dados qunado necessário
-function disconnectDataBase() {
-    connection.end();
-}
 
 /*função que irá preparar o banco de dados e 
-    as tabelas necessárias para a utilização da aplicação
-    caso o banco ainda não esteja preparado para tal.*/
-function dbInit() {
-    connectDataBase()
-    global.connection.
-        query('CREATE DATABASE IF NOT EXISTS ' + dbName)
-        .then(function () {
-            console.log("database is created: " + dbName);
-            global.connection.query("use " + dbName)
-        })
-        .then(function () {
-            console.log("Using " + dbName);
-            tablesInit();
-        })
-        .catch(error => {
-            console.log("Problem while creating database: " + error);
-        });
+    as tabelas necessárias para a utilização da aplicação .*/
+async function dbInit() {
+    try {
+        await connectDataBase()
+        console.log("Database connected...")
+        await global.connection.query('CREATE DATABASE IF NOT EXISTS ' + dbName);
+        console.log("Database is created: " + dbName);
+        await global.connection.query("use " + dbName);
+        console.log("Syncing tables...");
+        await tablesInit();
+        console.log("Tables are created.");
+    } catch (error) {
+        console.log("Problem while preparing database: " + error);
+    }
 }
 
-function tablesInit() {
-    const Products = globalconnection.define('produtos', {
+async function tablesInit() {
+
+    const Products = global.connection.define('produtos', {
         nome: { type: Sequelize.STRING },
         descricao: { type: Sequelize.TEXT },
-        preco: { type: Sequelize.NUMBER },
-        image: { type: Sequelize.STRING },
+        preco: { type: Sequelize.FLOAT },
+        imagem: { type: Sequelize.STRING },
         tags: { type: Sequelize.STRING },
         status: { type: Sequelize.STRING },
-        deleted: { type: Sequelize.BOOLEAN }
+        excluido: { type: Sequelize.BOOLEAN }
     });
 
-    const Cart = globalconnection.define('carrinho', {
-        nome: { type: Sequelize.STRING },
-        descricao: { type: Sequelize.TEXT },
-        preco: { type: Sequelize.NUMBER },
-        image: { type: Sequelize.STRING },
-        tags: { type: Sequelize.STRING },
-        status: { type: Sequelize.STRING },
-        deleted: { type: Sequelize.BOOLEAN }
+    const Cart = global.connection.define('carrinhos', {
+        idCliente: { type: Sequelize.STRING },
+        dataCriacao: { type: Sequelize.DATE },
+        horaCriacao: { type: Sequelize.TIME },
+        excluido: { type: Sequelize.BOOLEAN }
     });
 
-    const Order = globalconnection.define('pedidos', {
-        nome: { type: Sequelize.STRING },
-        descricao: { type: Sequelize.TEXT },
-        preco: { type: Sequelize.NUMBER },
-        image: { type: Sequelize.STRING },
-        tags: { type: Sequelize.STRING },
-        status: { type: Sequelize.STRING },
-        deleted: { type: Sequelize.BOOLEAN }
+    const CartItems = global.connection.define('itensCarrinho', {
+        idProduto: { type: Sequelize.STRING },
+        idCarrinho: { type: Sequelize.STRING },
+        quantidade: { type: Sequelize.INTEGER },
+        excluido: { type: Sequelize.BOOLEAN }
     });
+
+    const Order = global.connection.define('pedidos', {
+        formaPagamento: { type: Sequelize.STRING },
+        enderecoEntrega: { type: Sequelize.STRING },
+        dataCriacao: { type: Sequelize.DATE },
+        valorTotal: { type: Sequelize.FLOAT },
+        status: { type: Sequelize.STRING },
+        idCarrinho: { type: Sequelize.STRING },
+        excluido: { type: Sequelize.BOOLEAN }
+    });
+
+    const OrderItems = global.connection.define('itensPedido', {
+        idProduto: { type: Sequelize.TEXT },
+        nome: { type: Sequelize.STRING },
+        preco: { type: Sequelize.FLOAT },
+        quantidade: { type: Sequelize.INTEGER }
+    });
+
+    await Products.sync({ force: true });
+    await Cart.sync({ force: true });
+    await CartItems.sync({ force: true });
+    await Order.sync({ force: true });
+    await OrderItems.sync({ force: true });
 
 }
 
-module.exports = { connectDataBase, disconnectDataBase, dbInit };
+module.exports = { connectDataBase, dbInit };
